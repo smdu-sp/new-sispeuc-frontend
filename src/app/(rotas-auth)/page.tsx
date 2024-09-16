343d169a723ab08a967fbf4b516331037f85748a
gumends <gustavo08.mendes@gmail.com>
2024-09-16 16:10:42 -0300

    Tela inicial

--- src/app/(rotas-auth)/page.tsx
+++ src/app/(rotas-auth)/page.tsx
@@ -5,6 +5,17 @@ import { useSearchParams } from 'next/navigation';
 import * as usuarioServices from '@/shared/services/usuario.services';
 import { IUsuario } from '@/shared/services/usuario.services';
 import { useContext, useEffect, useState } from 'react';
+import { Box, Typography } from '@mui/joy';
+import CardImoveis from '@/components/CardImoveis';
+import GroupAddOutlinedIcon from '@mui/icons-material/GroupAddOutlined';
+import DomainAddIcon from '@mui/icons-material/DomainAdd';
+import LocationCityIcon from '@mui/icons-material/LocationCity';
+import FindInPageIcon from '@mui/icons-material/FindInPage';
+import CardParam from '@/components/CardParam';
+import Chart from '@/components/Chart';
+import PieArcLabel from '@/components/PieChart';
+import ChartHori from '@/components/ChartHori';
+import FormatTextdirectionRToLIcon from '@mui/icons-material/FormatTextdirectionRToL';
 
 export default function Home() {
 
@@ -22,6 +33,69 @@ export default function Home() {
       titulo={'Olá ' + nome + ', boas vindas ao SISPEUC 👋'}
       pagina='/'
     >
+      <Box sx={{ width: '100%', display: 'flex', gap: 2, mb: 2 }}>
+        <CardImoveis width='25%' icone={GroupAddOutlinedIcon} titulo="Prospecção" botao='Ver imóveis' corIcon='#283593' />
+        <CardImoveis width='25%' icone={LocationCityIcon} titulo="Cadastramento" botao='Ver imóveis' corIcon='#EE1D23' />
+        <CardImoveis width='25%' icone={FindInPageIcon} titulo="Vistoria" botao='Ver imóveis' corIcon='#FFAB00' />
+        <CardImoveis width='25%' icone={DomainAddIcon} titulo="Despacho" botao='Ver imóveis' corIcon='#1877F2' />
+      </Box>
+      <Box sx={{ width: '100%', display: 'flex', gap: 2, mb: 2 }}>
+        <CardParam width='25%' icone={LocationCityIcon} param='714K' descricao='Imóveis' corIcon='#EE1D23' />
+        <CardParam width='25%' icone={GroupAddOutlinedIcon} param='1.35m' descricao='prospectados' corIcon='#283593' />
+        <CardParam width='25%' icone={FormatTextdirectionRToLIcon} param='1.72m' descricao='Em preenchimento' corIcon='#FFAB00' />
+        <CardParam width='25%' icone={DomainAddIcon} param='234' descricao='Despachados' corIcon='#1877F2' />
+      </Box>
+      <Box sx={{ width: '100%', display: 'flex', gap: 2 }}>
+        <Box sx={{
+          width: '60%',
+          height: '485px',
+          bgcolor: 'background.body',
+          borderRadius: 15,
+          boxShadow: '0px 12px 24px -4px #919EAB1F',
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'start'
+        }}
+        >
+          <Box sx={{ pl: 5, pt: 3, mb: 3 }}>
+            <Typography level='h3'>Deúncias</Typography>
+            <Typography sx={{ color: '#637381' }} level="body-md" >(+43%) do que o ano passado</Typography>
+          </Box>
+          <Chart />
+        </Box>
+        <Box sx={{
+          width: '40%',
+          height: '485px',
+          bgcolor: 'background.body',
+          borderRadius: 15,
+          boxShadow: '0px 12px 24px -4px #919EAB1F',
+        }}
+        >
+          <Typography level='h3' sx={{ mt: 2, ml: 5 }}>
+            Visão geral imóveis
+          </Typography>
+          <PieArcLabel />
+        </Box>
+      </Box>
+      <Box sx={{
+        width: '100%',
+        height: '453px',
+        bgcolor: 'background.body',
+        borderRadius: 15,
+        boxShadow: '0px 12px 24px -4px #919EAB1F',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'start'
+      }}
+      >
+        <Box sx={{ pl: 5, pt: 3 }}>
+          <Typography level='h3'>Deúncias por fonte</Typography>
+          <Typography sx={{ color: '#637381' }} level="body-md" >(+43%) than last year</Typography>
+        </Box>
+        <Box sx={{ width: '100%', display: 'flex', mt: -2 }}>
+          <ChartHori />
+        </Box>
+      </Box>
     </Content>
   );
 }
