96b1339d993b7ed333579d679d53cc414515c034
Marcus-Nastasi <vinnie.nsts@gmail.com>
2024-11-01 12:56:07 -0300

    implementando download controlado de pdf no modal de arquivos de vistoria

--- src/components/VistoriaFilesCarousel.tsx
+++ src/components/VistoriaFilesCarousel.tsx
@@ -6,10 +6,12 @@ import { useTheme } from '@mui/material/styles';
 import MobileStepper from '@mui/material/MobileStepper';
 import Paper from '@mui/material/Paper';
 import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
 import KeyboardArrowLeft from '@mui/icons-material/KeyboardArrowLeft';
 import KeyboardArrowRight from '@mui/icons-material/KeyboardArrowRight';
+import DownloadForOfflineRoundedIcon from '@mui/icons-material/DownloadForOfflineRounded';
 import { VistoriaAssetDto } from '@/types/vistorias/vistorias.dto';
+import { Button as ButtonJoy } from '@mui/joy';
+import { Button } from '@mui/material';
 
 export default function VistoriaFilesCarousel(
     { vistoriaAssets, selectedIndex, setSelectedIndex }: 
@@ -57,11 +59,39 @@ export default function VistoriaFilesCarousel(
             overflowY: 'scroll'
         }}
       >
-        <img 
-            style={{ width: '800px', padding: 5 }}     
-            src={vistoriaAssets[selectedIndex ? selectedIndex : 0].url} 
-            alt={vistoriaAssets[selectedIndex ? selectedIndex : 0].nomeArquivo} 
-        />
+        {vistoriaAssets[selectedIndex].nomeArquivo.split('.').pop()?.toLocaleLowerCase() == 'pdf' 
+        &&  <>
+                <iframe
+                    src={vistoriaAssets[selectedIndex ? selectedIndex : 0].url}
+                    title="Visualizador de PDF"
+                    sandbox="allow-same-origin allow-scripts"
+                    style={{ 
+                        width: '100%', 
+                        height: '100%' 
+                    }}
+                ></iframe>
+                <a 
+                    href={vistoriaAssets[selectedIndex ? selectedIndex : 0].url} 
+                    download={vistoriaAssets[selectedIndex ? selectedIndex : 0].nomeArquivo}
+                >
+                    <ButtonJoy
+                        variant='solid'
+                        color='primary'
+                        startDecorator={<DownloadForOfflineRoundedIcon />}
+                        sx={{
+                            my: 1,
+                        }}
+                    >
+                        Download
+                    </ButtonJoy>
+                </a>
+            </>
+        ||  <img 
+                style={{ width: '800px', padding: 5 }}     
+                src={vistoriaAssets[selectedIndex ? selectedIndex : 0].url} 
+                alt={vistoriaAssets[selectedIndex ? selectedIndex : 0].nomeArquivo} 
+            /> 
+        }
       </Box>
       <MobileStepper
         variant="text"
